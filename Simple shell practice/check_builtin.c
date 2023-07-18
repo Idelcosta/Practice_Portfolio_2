56bc41dd9a81331cb5080025591a0f8b13cc25a9
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-18 12:36:58 +0300

    SIM JF

--- Simple shell practice/check_builtin.c
+++ Simple shell practice/check_builtin.c
@@ -3,7 +3,7 @@
 bool check_builtin(char **args, size_t args_count)
 {
     if (strncmp(args[0], "exit", 4) == 0)
-        exit_function(args, args_count);
+        exit_function(args);
 
     else if ((strncmp(args[0], "env", 3) == 0) || (strncmp(args[0], "printenv", 8 == 0)))
     {
