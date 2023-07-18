d75c3f9acca6d2adbfa3f3001c36f9b65462a71c
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-18 03:27:47 +0300

    SIM JF

--- Simple shell practice/get_path.c
+++ Simple shell practice/get_path.c
@@ -11,7 +11,7 @@ char *get_location(char *command)
     if (path) {
         path_copy = strdup(path);
         command_length = strlen(command);
-        path_token = strtok(path_copy, ";");
+        path_token = strtok(path_copy, ":");
 
         while (path_token != NULL){
 
