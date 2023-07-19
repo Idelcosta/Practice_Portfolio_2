699b848b338debfc573e7465643b12eb59c4efb4
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-19 05:38:52 +0300

    simple shell

--- Simple shell practice/get_path.c
+++ Simple shell practice/get_path.c
@@ -3,8 +3,9 @@
 char *get_location(char *command)
 {
     char *path, *path_copy, *path_token, *file_path;
-    int command_length, directory_length;
+    int command_length, directory_length, del1_count, del2_count;
     struct stat buffer;
+    char del1 = "/", del2 = "\0";
 
     path = getenv("PATH");
 
@@ -16,12 +17,14 @@ char *get_location(char *command)
         while (path_token != NULL){
 
             directory_length = strlen(path_token);
+            del1_count = strlen(del1);
+            del2_count = strlen(del2);
             file_path = malloc(command_length + directory_length + 2);
 
             _strncpy(file_path, path_token, directory_length);
-            _strncat(file_path, "/", 1);
+            _strncat(file_path, del1, del1_count);
             _strncat(file_path, command, command_length);
-            _strncat(file_path, "\0", 2);
+            _strncat(file_path, del2, del2_count);
 
             if (stat(file_path, &buffer) == 0){
                 free(path_copy);
