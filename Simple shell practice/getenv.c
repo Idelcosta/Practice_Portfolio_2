17ba03f2fe5c57371ac2d9dc6201b74cb05c3dcc
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-19 20:24:09 +0300

    simple shell

--- Simple shell practice/getenv.c
+++ Simple shell practice/getenv.c
@@ -1,6 +1,25 @@
 #include "shell_library.h"
 
 /**
+ * compare - function that compares two strings
+ * @varname: is a char
+ * @dirname: is a char
+ * Return: 1
+ */
+
+int compare(char *varname, char *dirname)
+{
+	int i;
+
+	for (i = 0; dirname[i] != '\0'; i++)
+	{
+		if (dirname[i] != varname[i])
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * length - function that checks the = character
  * @str: is a char
  * Return: str
@@ -35,7 +54,7 @@ char *_getenv(char **environ, char *dirname)
 		for (k = 0; environ[j][k] != '='; k++)
 			varname[k] = environ[j][k];
 
-		if (_strcmp(varname, dirname))
+		if (compare(varname, dirname))
 		{
 			final = length(environ[j]);
 			free(varname);
