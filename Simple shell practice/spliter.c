557e51834731fb5956abda7c0a65bff56830a432
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-19 21:31:29 +0300

    simple shell

---
+++ Simple shell practice/spliter.c
@@ -0,0 +1,26 @@
+#include "shell_library.h"
+
+char* my_strtok(char* str, char delim) {
+    static char* nextTokenStart = NULL;
+    char* tokenStart = (str != NULL) ? str : nextTokenStart;
+    char* tokenEnd;
+    
+    if (tokenStart == NULL || *tokenStart == '\0') {
+        return NULL; 
+    }
+    
+    tokenEnd = tokenStart;
+    
+    while (*tokenEnd != delim && *tokenEnd != '\0') {
+        tokenEnd++;
+    }
+    
+    if (*tokenEnd == delim) {
+        *tokenEnd = '\0';  
+        nextTokenStart = tokenEnd + 1;  
+    } else {
+        nextTokenStart = NULL;  
+    }
+    
+    return tokenStart;
+}
