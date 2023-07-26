cf397072389a5c64ac4839bf8b8fb8eaf3944d80
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-26 22:01:45 +0300

    simple shell

--- Simple shell practice/prompt.c
+++ Simple shell practice/prompt.c
@@ -9,30 +9,38 @@
 */
 int main(int ac, char **argv)
 {
-    char *string = NULL, *string_copy = NULL, *arguments;
-    int i, status, string_length, arguments_count = 0;
-    size_t n = 0, number_character;
+    char *string = NULL, *string_copy = NULL;
+    int i, status, string_length;
+    size_t n = 0;
+    ssize_t number_character;
     pid_t child_pid;
+    char *arguments;
+    int arguments_count = 0;
+
     (void)ac;
 
     while (1)
     {
+
         if (isatty(STDIN_FILENO))
         {
             printf ("cisfun$ ");
         }
+
         number_character = getline(&string, &n, stdin);
         if (number_character == -1)
         {
             free(string); 
             exit(EXIT_FAILURE);
         }
+
         string_copy = malloc(sizeof(char) * number_character);
         if (string_copy == NULL)
         {
             perror("error memory allocation");
             return (-1);
         }
+
         string_length = _strlen(string);
         _strncpy(string_copy, string, string_length);
         arguments = strtok(string, " \n");
@@ -42,8 +50,10 @@ int main(int ac, char **argv)
             arguments = strtok(NULL, " \n");
         }
         arguments_count++;
+
         argv = malloc(sizeof(char *) * arguments_count);
         arguments = strtok(string_copy, " \n");
+
         for (i = 0; arguments != NULL; i++){
             argv[i] = malloc(sizeof(char) * _strlen(arguments));
             _strncpy(argv[i], arguments, string_length);
@@ -51,35 +61,26 @@ int main(int ac, char **argv)
             arguments = strtok(NULL, " \n");
         }
         argv[i] = NULL;
-        void pid_execution()
+
+        if (check_builtin(argv, number_character))
+            continue;
+
+        child_pid = fork();
+        if (child_pid == -1)
+        {
+            free(string);
+            exit(EXIT_FAILURE);
+        }
+        if (child_pid == 0)
+        {
+            execution_command(argv);
+        }
+        else
+            wait(&status);
     }
+
     free(string);
     free(string_copy);
+
     return(0);
-}
-
-/**
-*pid_executiont: create child process and execute the commands
-*/
-void pid_execution()
-{
-    char **argv;
-    size_t n = 0, number_character;
-    char *string = NULL;
-
-    if (check_builtin(argv, number_character))
-        continue;
-
-    child_pid = fork();
-    if (child_pid == -1)
-    {
-        free(string);
-        exit(EXIT_FAILURE);
-    }
-    if (child_pid == 0)
-    {
-        execution_command(argv);
-    }
-    else
-        wait(&status);
 }
