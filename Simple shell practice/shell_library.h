8edadd7d1f5c85f33abf3ec2e1975e2399ad2cd7
Idelcosta <mbutuyeidelcosta@gmail.com>
2023-07-26 21:57:31 +0300

    simple shell

--- Simple shell practice/shell_library.h
+++ Simple shell practice/shell_library.h
@@ -18,6 +18,7 @@ int _atoi(char *s);
 void exit_function(char **exit_args);
 bool check_builtin(char **args, size_t args_count);
 void env_list();
+void pid_execution();
 char *_strncpy(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
